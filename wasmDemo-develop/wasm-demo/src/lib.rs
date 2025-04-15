10386e1baa58e5f3365979650bde339c691f97ea
Mikkel <146935836+minkguel@users.noreply.github.com>
2025-04-15 20:32:00 +0200

    Succesfully displayed a string in browser with rust and wasm

--- wasmDemo-develop/wasm-demo/src/lib.rs
+++ wasmDemo-develop/wasm-demo/src/lib.rs
@@ -1,89 +1,35 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use std::rc::Rc;
-use std::cell::RefCell;
-use web_sys::{FileReader, HtmlElement, Event, DragEvent};
-use async_std::net::UdpSocket;
+use web_sys::{WebSocket, MessageEvent, console, window, Document, Element};
 
+// Establishes a WebSocket connection to a server and listens for messages
+pub async fn start_websocket() {
+   let ws = WebSocket::new("ws://localhost:8080").unwrap();
 
-#[wasm_bindgen]
-pub fn getTemp() {
-    #[async_std::main]
-    async fn main() -> std::io::Result<()> {
-        {
-            let socket = UdpSocket::bind("127.0.0.1:8080").await?;
-            println!("Listening on {}", socket.local_addr()?);
-
-            let mut buf = vec![0u8; 1024];
-
-            loop
-            {
-                let (recv, peer) = socket.recv_from(&mut buf).await?;
-                let data = i32::from_be_bytes(buf[0..4].try_into().unwrap());
-
-                println!("Received {} from {:?}", data, peer);
-            }
+    // data refers to the data received as Rust code
+    let onmessage_callback = Closure::wrap(Box::new(move |event: MessageEvent| {
+        if let Ok(data) = event.data().dyn_into::<js_sys::JsString>() {
+            let data = data.as_string().unwrap_or_default();
+            web_sys::console::log_1(&format!("Received: {}", data).into());
         }
-    }
-}
+    }) as Box<dyn FnMut(_)>);
 
+    // Registers the onmessage callback. forget() is used to prevent the closure from being garbage collected.
+    ws.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+    onmessage_callback.forget();
+}
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn display_string_in_browser(message: &str) {
+    let window = window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+
+    // Create a new div element
+    let div = document.create_element("div").expect("should create a div");
+
+    // Set the div inner HTML to the message
+    div.set_inner_html(message);
+
+    document.body().expect("document should have a body").append_child(&div).expect("Should append div to body");
 }
 
-#[wasm_bindgen]
-pub fn setup_drag_and_drop(drop_zone_id: &str, output_id: &str) {
-    let window = web_sys::window().expect("No window found");
-    let document = window.document().expect("No document found");
 
-    let drop_zone = document.get_element_by_id(drop_zone_id)
-        .expect("Drop zone not found")
-        .dyn_into::<HtmlElement>()
-        .expect("Could not cast to HtmlElement");
-
-    let output = document.get_element_by_id(output_id)
-        .expect("Output not found");
-    let output = Rc::new(RefCell::new(output));
-
-    let closure = {
-        let output = Rc::clone(&output);
-
-        Closure::wrap(Box::new(move |event: DragEvent| {
-            event.prevent_default();
-            if let Some(data_transfer) = event.data_transfer() {
-                if let Some(files) = data_transfer.files() {
-                    if let Some(file) = files.item(0) {
-                        let file_name = file.name();
-                        let file_size = file.size();
-
-                        let reader = FileReader::new().expect("Failed to create FileReader");
-                        let reader_clone = reader.clone();
-
-                        let output_clone = Rc::clone(&output);
-
-                        let onload_callback = Closure::wrap(Box::new(move |_event: Event| {
-                            let result = reader_clone.result().unwrap();
-                            let url = result.as_string().unwrap();
-                            
-                            output_clone.borrow().set_inner_html(&format!(
-                                "<p>File: {} ({} bytes)</p><img src='{}' style='max-width: 200px;'/>",
-                                file_name, file_size, url
-                            ));
-                        }) as Box<dyn FnMut(_)>);
-
-                        reader.set_onload(Some(onload_callback.as_ref().unchecked_ref()));
-                        reader.read_as_data_url(&file).expect("Failed to read file");
-
-                        onload_callback.forget();
-                    }
-                }
-            }
-        }) as Box<dyn FnMut(_)>)
-    };
-
-    drop_zone.set_ondrop(Some(closure.as_ref().unchecked_ref()));
-    drop_zone.set_ondragover(Some(closure.as_ref().unchecked_ref()));
-
-    closure.forget();
-}
